50aa5e29d622c2e76b1c4a6c3be67b54e299bbd6
Tony Bird <tonybird@live.unc.edu>
2018-07-17 17:54:28 -0400

    Added server-side pagination

--- shortcode.php
+++ shortcode.php
@@ -6,7 +6,7 @@
 add_shortcode('bibliography', 'display_entries');
 
 function display_entries() {
-  echo "<h3>Using DataTables:</h3>";
+
   //the query
 //   $args = array(
 //   'post_type' => 'bib',
@@ -31,7 +31,7 @@ $args = array(
 $args = array(
   'post_type' => 'bib',
   'post_status'=>'publish',
-  'posts_per_page'=>-1,
+  'posts_per_page'=>10,
     'meta_query' => array(
       'relation' => 'AND',
       'year_clause' => array(
@@ -68,49 +68,98 @@ $args = array(
 //   )
 // );
 
+  // $wpb_all_query = new WP_Query($args);
+  //
+  //
+  //     echo "<table>";
+  //     while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post();
+  //     global $post;
+  //
+  //     if (get_field('citation') !== "") {
+  //       echo "<tr>
+  //       <td>$post->citation</td>
+  //       </tr>";
+  //     }
+  //   endwhile;
+  //   echo "</table>";
+  //
 
+    // Define custom query parameters
+    $custom_query_args = $args;
 
+    // Get current page and append to custom query parameters array
+    $custom_query_args['paged'] = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
 
+    // Instantiate custom query
+    $custom_query = new WP_Query( $custom_query_args );
 
-  $wpb_all_query = new WP_Query($args);
-  if ( $wpb_all_query->have_posts() ) :
-    echo "<link rel='stylesheet' type='text/css' href='https://cdn.datatables.net/v/dt/jq-3.3.1/dt-1.10.18/datatables.min.css'/>
+    // Pagination fix
+    $temp_query = $wp_query;
+    $wp_query   = NULL;
+    $wp_query   = $custom_query;
 
-<script type='text/javascript' src='https://cdn.datatables.net/v/dt/jq-3.3.1/dt-1.10.18/datatables.min.js'></script>
+    // Output custom query loop
+    if ( $custom_query->have_posts() ) :
+        while ( $custom_query->have_posts() ) :
+            $custom_query->the_post();
+            // Loop output goes here
+            global $post;
+            echo "<p>";
+            echo $post->citation;
+            echo "</p>";
+        endwhile;
+    endif;
+    // Reset postdata
+    wp_reset_postdata();
 
+    // Custom query loop pagination
+    previous_posts_link( '< Previous' );
+    echo " ";
+    next_posts_link( 'Next >', $custom_query->max_num_pages );
 
-    ";
+    // Reset main query object
+    $wp_query = NULL;
+    $wp_query = $temp_query;
 
-    echo "<table id='citations' class='table'>
-           <thead style='display:none'>
-             <tr>
-               <th>Citations</th>
-             </tr>
-           </thead>
-           <tbody>";
-
-    while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post();
-    global $post;
-
-    if (get_field('citation') !== "") {
-      echo "<tr>
-      <td>$post->citation</td>
-      </tr>";
-    }
-  endwhile;
-  echo "</tbody>
-  </table>
-
-  <script type='text/javascript'>
-
-  $('#citations').DataTable();
-
-
-  </script>";
-
-  wp_reset_postdata();
-  else : echo "<p>Sorry, no bibliography entries were found.</p>";
-endif;
+// DATATABLES
+//   if ( $wpb_all_query->have_posts() ) :
+//     echo "<link rel='stylesheet' type='text/css' href='https://cdn.datatables.net/v/dt/jq-3.3.1/dt-1.10.18/datatables.min.css'/>
+//
+// <script type='text/javascript' src='https://cdn.datatables.net/v/dt/jq-3.3.1/dt-1.10.18/datatables.min.js'></script>
+//
+//
+//     ";
+//
+//     echo "<table id='citations' class='table'>
+//            <thead style='display:none'>
+//              <tr>
+//                <th>Citations</th>
+//              </tr>
+//            </thead>
+//            <tbody>";
+//
+//     while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post();
+//     global $post;
+//
+//     if (get_field('citation') !== "") {
+//       echo "<tr>
+//       <td>$post->citation</td>
+//       </tr>";
+//     }
+//   endwhile;
+//   echo "</tbody>
+//   </table>";
+//
+//   echo "<script type='text/javascript'>
+//
+//   $('#citations').DataTable();
+//
+//
+//   </script>";
+//
+//   wp_reset_postdata();
+//   else : echo "<p>Sorry, no bibliography entries were found.</p>";
+// endif;
 
 }
 
