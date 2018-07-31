925b51ad661e73e3132efa3ddc11af89b378306d
Tony Bird <tonybird@live.unc.edu>
2018-07-31 14:41:33 -0400

    Removed unused test import shortcode

--- cpc-bibliography.php
+++ cpc-bibliography.php
@@ -9,7 +9,7 @@ Author: Tony Bird
 
 
 
-include( plugin_dir_path( __FILE__ ) . 'reflib-test.php');
+//include( plugin_dir_path( __FILE__ ) . 'reflib-test.php');
 //include( plugin_dir_path( __FILE__ ) . 'citation-on-publish.php');
 include( plugin_dir_path( __FILE__ ) . 'importer-page.php');
 
